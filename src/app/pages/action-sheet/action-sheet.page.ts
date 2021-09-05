aa5b8d8024a78720d4fee4c0ca51c4f91028559c
Jhonnathan Rodriguez <jhrrodrig@gmail.com>
2021-09-05 18:57:17 -0300

    se realizan pruebas de aler y action-sheet

--- src/app/pages/action-sheet/action-sheet.page.ts
+++ src/app/pages/action-sheet/action-sheet.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-action-sheet',
@@ -7,9 +8,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActionSheetPage implements OnInit {
 
-  constructor() { }
+  constructor(private actionSheetCtrl: ActionSheetController) { }
 
   ngOnInit() {
   }
 
+  onClick(){    
+    this.presentActionSheet();
+  }
+
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: 'Opciones',
+      backdropDismiss: false,     
+      buttons: [{
+        text: 'Borrar',
+        role: 'destructive',
+        icon: 'trash',        
+        cssClass: 'rojo',
+        handler: () => {
+          console.log('Borrar clicked');
+        }
+      }, {
+        text: 'Compartir',
+        icon: 'share',
+        handler: () => {
+          console.log('Compartir clicked');
+        }
+      }, {
+        text: 'Play (open modal)',
+        icon: 'caret-forward-circle',
+        handler: () => {
+          console.log('Play clicked');
+        }
+      }, {
+        text: 'Favorito',
+        icon: 'heart',
+        handler: () => {
+          console.log('Favorito clicked');
+        }
+      }, {
+        text: 'Cancelar',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancelar clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
+  };
 }
